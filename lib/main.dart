17ebcf3dc09bf01bd101bdba26b8bf3386a89211
Oskar Wieczorek <oziw57@gmail.com>
2025-11-07 13:18:41 +0000

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sandwich_shop/views/app_styles.dart';
+import 'views/app_styles.dart';
 
 void main() {
   runApp(const App()); //Hi Ozi <3 I love you so much :) - A.W
